ec8122b144e9c0178ec1c3a26fb54b24cf033023
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-09-23 23:03:48 +0530

    Added Logout Logic

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -135,4 +135,31 @@ const loginUser = asyncHandler(async(req,res)=>{
 
 })
 
-export {registerUser,loginUser}
+const logoutUser = asyncHandler(async(req,res)=>{
+    await User.findByIdAndUpdate(
+        req.user._id,
+        {
+            $set:{
+                refreshToken:undefined
+            }
+        },
+        {
+            new:true
+        }
+    )
+
+    const options = {
+        httpOnly:true,
+        secure:true
+    }
+
+    return res
+    .status(200)
+    .clearCookie("accessToken",options)
+    .clearCookie("refreshToken",options)
+    .json(
+        new ApiResponse(200,{},"User Logged Out")
+    )
+})
+
+export {registerUser,loginUser,logoutUser}
