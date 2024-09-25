70a793f4051eab6a47fe9f2467d9307d3a38d25c
Aditya Yadav <officialuser.aditya2022@gmail.com>
2024-09-25 23:41:01 +0530

    Added Routes for the user controller functions

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -235,7 +235,9 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res)=>{
     return res
     .status(200)
-    .json(200,req.user,"Current user fetched successfully")
+    .json(
+        new ApiResponse(200,req.user,"Current user fetched successfully")
+    )
 })
 
 const updateAccountDetails = asyncHandler(async(req,res)=>{
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import {Router} from "express"
-import {loginUser, logoutUser, registerUser,refreshAccessToken} from "../controllers/user.controller.js"
+import {
+    loginUser,
+    logoutUser,
+    registerUser,
+    refreshAccessToken,
+    changeCurrentPassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage
+} from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
 
@@ -23,5 +33,10 @@ router.route("/login").post(loginUser)
 //Secured routes
 router.route("/logout").post(verifyJWT,logoutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 
 export default router
