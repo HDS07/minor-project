00d075976c28f5cfa521614dca3ae978f7330bc2
HDS07 <hs4205170@gmail.com>
2024-10-22 01:23:49 +0530

    Modified some file and Bugs

--- Backend/src/controllers/user.controller.js
+++ Backend/src/controllers/user.controller.js
@@ -1,7 +1,6 @@
 import {asyncHandler} from "../utils/asyncHandler.js"
 import {ApiError} from "../utils/ApiError.js"
 import {User} from "../models/user.model.js"
-import {destroyFromCloudinary, uploadOnCloudinary} from "../utils/Cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
 
@@ -46,30 +45,8 @@ const registerUser = asyncHandler(async(req,res)=>{
     if(existedUser){
         throw new ApiError(409,"User with email or username already exists")
     }
-    // const avatarLocalPath = req.files?.avatar[0]?.path
-    // //const coverImageLocalPath = req.files?.coverImage[0]?.path
-    // let coverImageLocalPath;
-    // if(req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length>0){
-    //     coverImageLocalPath = req.files.coverImage[0].path
-    // }
-
-    // if(!avatarLocalPath){
-    //     throw new ApiError(400,"Avatar file is required")
-    // }
-
-    // const avatar = await uploadOnCloudinary(avatarLocalPath)
-    // const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-
-    // if(!avatar){
-    //     throw new ApiError(400,"Avatar file is required")
-    // }
-
     const user = await User.create({
         fullName,
-        // avatar: avatar.url?.url||"",
-        // coverImage: coverImage?.url || "",
-        avatar:"",
-        coverImage:"",
         email,
         password,
         username: username.toLowerCase()
@@ -265,75 +242,6 @@ const updateAccountDetails = asyncHandler(async(req,res)=>{
     return res.status(200)
     .json(new ApiResponse(200,user,"Account details Updated Successfully"))
 })
-
-const updateUserAvatar = asyncHandler(async(req,res)=>{
-    const avatarLocalPath = req.file?.path
-
-    if(!avatarLocalPath){
-        throw new ApiError(400,"Avatar file is missing")
-    }
-
-    await destroyFromCloudinary(req.user.avatar)
-
-    const avatar = await uploadOnCloudinary(avatarLocalPath)
-
-    if(!avatar.url){
-        throw new ApiError(400,"Error while uploading on avatar")
-    }
-
-    const user = await User.findByIdAndUpdate(
-        req.user?._id,
-        {
-            $set:{
-                avatar:avatar.url
-            }
-        },
-        {new:true}
-    ).select("-password")
-
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,user,"Avatar updated successfully")
-    )
-
-})
-
-const updateUserCoverImage = asyncHandler(async(req,res)=>{
-    const coverImageLocalPath = req.file?.path
-
-    if(!coverImageLocalPath){
-        throw new ApiError(400,"Cover Image file is missing")
-    }
-
-    if(req.user.coverImage!=""){
-        await destroyFromCloudinary(req.user.coverImage)
-    }
-
-    const coverImage = await uploadOnCloudinary(coverImageLocalPath)
-
-    if(!coverImage.url){
-        throw new ApiError(400,"Error while uploading on avatar")
-    }
-
-    const user = await User.findByIdAndUpdate(
-        req.user?._id,
-        {
-            $set:{
-                coverImage:coverImage.url
-            }
-        },
-        {new:true}
-    ).select("-password")
-
-    return res
-    .status(200)
-    .json(
-        new ApiResponse(200,user,"Cover Image updated successfully")
-    )
-
-})
-
 export {
     registerUser,
     loginUser,
@@ -341,7 +249,5 @@ export {
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
-    updateAccountDetails,
-    updateUserAvatar,
-    updateUserCoverImage
+    updateAccountDetails
 }
--- Backend/src/routes/user.routes.js
+++ Backend/src/routes/user.routes.js
@@ -6,9 +6,7 @@ import {
     refreshAccessToken,
     changeCurrentPassword,
     getCurrentUser,
-    updateAccountDetails,
-    updateUserAvatar,
-    updateUserCoverImage
+    updateAccountDetails
 } from "../controllers/user.controller.js"
 import {upload} from "../middlewares/multer.middleware.js"
 import {verifyJWT} from "../middlewares/auth.middleware.js"
@@ -36,7 +34,5 @@ router.route("/refresh-token").post(refreshAccessToken)
 router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-account").patch(verifyJWT,updateAccountDetails)
-router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
 
 export default router
